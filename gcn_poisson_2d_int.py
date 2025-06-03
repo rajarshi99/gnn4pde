3254460cbf33269813e5d4eeaad6320f72ae84a9
Rajarshi <rajarshi99@gmail.com>
2025-06-03 20:51:58 +0530

    Changed eg prob

--- gcn_poisson_2d_int.py
+++ gcn_poisson_2d_int.py
@@ -17,20 +17,29 @@ import triangle as tr
 import sys
 
 import matplotlib.pyplot as plt
+import time
+
+omegax = 2*jnp.pi
+omegay = 2*jnp.pi
+r1 = 10
 
 def u(x,y):
     # return jnp.where(y == 1, 1, 0)
     # return jnp.sin(jnp.pi*x)*jnp.sin(jnp.pi*y)
     # return x**2*y**2
     # return x*y
-    return 0.25*(1 - x*x - y*y)
+    # return 0.25*(1 - x*x - y*y)
+    return (0.1*jnp.sin(omegax*x) + jnp.tanh(r1*x)) * jnp.sin(omegay*(y))
 
 def f(x,y):
     # return 0
     # return 2*(jnp.pi)**2*jnp.sin(jnp.pi*x)*jnp.sin(jnp.pi*y)
     # return -2*(x**2 + y**2)
     # return 0
-    return 1
+    # return 1
+    gtemp = (-0.1*(omegax**2)*jnp.sin(omegax*x) - (2*r1**2)*(jnp.tanh(r1*x))/((jnp.cosh(r1*x))**2))*jnp.sin(omegay*(y))\
+            +(0.1*jnp.sin(omegax*x) + jnp.tanh(r1*x)) * (-omegay**2 * jnp.sin(omegay*(y)) )
+    return -gtemp
 
 def main(
             num_points,
@@ -90,16 +99,27 @@ def main(
         res = Kf[:,:-1] @ u - Kf[:,-1:]
         return jnp.sum(res*res)
 
+    vert_unknown_list = jnp.array(p_2d.vert_unknown_list)
+    u_exct_int = u_exct.at[vert_unknown_list].get()
+
+    def rel_l2_err_fn(u):
+        err = u.flatten() - u_exct_int.flatten()
+        return jnp.linalg.norm(err) / jnp.linalg.norm(u_exct)
+
 # Training of the GCN model
     history_list = []
+    start_time = time.time()
     for _ in range(num_fits):
-        model = GCNModel(gcn, loss_fn)
+        model = GCNModel(gcn, loss_fn, [rel_l2_err_fn])
         gcn, history = model.fit(u_gcn, A, deg, Kf,
                                  learning_rate=learning_rate,
                                  num_iters=iters_per_fit,
                                  num_check_points=iters_per_fit)
         history_list.append(history)
         u_gcn = gcn(u_gcn, A, deg)
+    elapsed_time = time.time() - start_time
+    print("Training time: ", elapsed_time)
+
     u_gcn = p_2d.assemble_sol(u_gcn.reshape(-1))
 
     p_2d.plot_on_mesh(u_gcn,
@@ -148,6 +168,36 @@ def main(
     plt.savefig(f"{output_dir}{num_points}_loss_gcn.png")
     plt.close()
 
+    for history_id,history in enumerate(history_list):
+        iter_ids = history["iter_ids"]
+        iter_ids += history_id*iters_per_fit
+        loss_vals = history["loss_vals"]
+        rel_l2_err_vals = history["metric_vals"][:,0]
+        plt.scatter(loss_vals, rel_l2_err_vals,
+                    c=iter_ids, cmap='jet')
+    plt.xlabel("Loss")
+    plt.ylabel("Relative l2 loss")
+    plt.colorbar()
+    plt.grid()
+    plt.savefig(f"{output_dir}{num_points}_loss_err.png")
+    plt.close()
+
+    for history_id,history in enumerate(history_list):
+        iter_ids = history["iter_ids"]
+        iter_ids += history_id*iters_per_fit
+        loss_vals = history["loss_vals"]
+        rel_l2_err_vals = history["metric_vals"][:,0]
+        plt.scatter(loss_vals, rel_l2_err_vals,
+                    c=iter_ids, cmap='jet')
+    plt.xlabel("Loss")
+    plt.ylabel("Relative l2 loss")
+    plt.colorbar()
+    plt.grid()
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.savefig(f"{output_dir}{num_points}_loss_err_log.png")
+    plt.close()
+
     jnp.savez(f"{output_dir}details.npz",
               vertices = domain['vertices'],
               triangles = domain['triangles'],
@@ -159,10 +209,25 @@ def main(
               iter_ids = jnp.array([ h["iter_ids"] for h in history_list]),
               )
 
-    relative_l2_error = \
+    gcn_relative_l2_error = \
             jnp.linalg.norm(u_gcn - u_exct) / jnp.linalg.norm(u_exct)
+    gcn_l_inf_error = jnp.max(jnp.abs(u_gcn - u_exct))
 
-    return relative_l2_error, history_list
+    fem_relative_l2_error = \
+            jnp.linalg.norm(u_fem - u_exct) / jnp.linalg.norm(u_exct)
+    fem_l_inf_error = jnp.max(jnp.abs(u_fem - u_exct))
+
+    print("gcn rel l2 error: ", gcn_relative_l2_error)
+    print("gcn l inf error: ", gcn_l_inf_error)
+
+    fem_relative_l2_error = \
+            jnp.linalg.norm(u_fem - u_exct) / jnp.linalg.norm(u_exct)
+    fem_l_inf_error = jnp.max(jnp.abs(u_fem - u_exct))
+
+    print("fem rel l2 error: ", fem_relative_l2_error)
+    print("fem l inf error: ", fem_l_inf_error)
+
+    return history_list
 
 if __name__ == "__main__":
     try:
@@ -173,11 +238,12 @@ if __name__ == "__main__":
     plt.rcParams['font.size'] = 18
     plt.rcParams['savefig.bbox'] = 'tight'
 
-    rel_l2_err, h_list = main(
+    h_list = main(
             num_points,
-            gcn_layers = [1] + [30]*3 + [1],
-            iters_per_fit = 10000,
+            gcn_layers = [1] + [40]*3 + [1],
+            iters_per_fit = 250000,
+            # iters_per_fit = 9500,
             num_fits = 1,
-            learning_rate = 0.0005,
-            output_dir = "apr14/"
+            learning_rate = 0.0001,
+            output_dir = "may4/"
             )
