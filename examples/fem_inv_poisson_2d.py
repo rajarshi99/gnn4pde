88f81fb08f73458a9e85c5265f7236a533945ba9
Rajarshi <rajarshi99@gmail.com>
2025-11-11 08:26:51 +0530

    fem inv prob working

--- examples/fem_inv_poisson_2d.py
+++ examples/fem_inv_poisson_2d.py
@@ -13,7 +13,11 @@ import triangle as tr
 
 import matplotlib.pyplot as plt
 
+from golden_section_search import golden_section_search
+
 from core.poisson_2d import Poisson_2d
+
+import sys
 
 def u(x,y):
     # return jnp.where(y == 1, 1, 0)
@@ -29,7 +33,10 @@ def f_guess(x,y):
     # return 0
     return 1
 
-num_points = 5
+try:
+    num_points = int(sys.argv[1])
+except:
+    num_points = 5
 
 output_dir = "output/"                           # folder to save the plots 
 
@@ -44,36 +51,35 @@ xy_coords = jnp.column_stack((x,y))
 domain = {'vertices' : xy_coords}
 for key, val in tr.triangulate(domain).items():
     domain[key] = val
-    print(key, val.shape)
 
 p_2d = Poisson_2d(domain, u, f_guess)
 K_mat, f_force, f_bound = p_2d.get_K_f1_f2()
 
-print(K_mat.shape, f_force.shape, f_bound.shape)
-
 dof_inds = jnp.array(p_2d.vert_unknown_list)
 xy_int = jnp.column_stack((x[dof_inds],y[dof_inds]))
 int_data_ind = jnp.where( (xy_int == jnp.array([0.5,0.5])).all(axis=1) )
-u_int_data = u(x[int_data_ind], y[int_data_ind])
-
-print("Internal data info", int_data_ind, x[int_data_ind], y[int_data_ind])
+u_int_data = u(xy_int[int_data_ind,0], xy_int[int_data_ind,1])
 
 def loss(f_val):
     u_tmp = jnp.linalg.solve(K_mat, f_val*f_force - f_bound)
-    print(u_tmp.shape)
     deviation = u_tmp[int_data_ind] - u_int_data
     return jnp.sum(deviation*deviation)
+
+f_optim = golden_section_search(loss, 0.8, 1.8)
 
 f_val_list = jnp.linspace(0.8,1.8)
 loss_val_list = []
 for f_val in f_val_list:
     loss_val_list.append(loss(f_val))
+loss_val_list = jnp.array(loss_val_list)
+
 plt.plot(f_val_list,loss_val_list)
 plt.xlabel("f")
 plt.ylabel("loss")
+
+plt.axvline(x=f_optim, linestyle="--")
 plt.grid()
-plt.savefig(f"{output_dir}/{num_points}fem_inv_loss_f.png")
+plt.savefig(f"{output_dir}/{num_points}_fem_inv_loss_f.png")
 plt.close()
 
-
-
+print(num_points, f_optim)
